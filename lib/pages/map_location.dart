7d4523411a523b8615c697c374867925a07a37f1
supyall2077 <83783895+supyall2077@users.noreply.github.com>
2024-03-03 11:59:36 -0800

    final_project

--- lib/pages/map_location.dart
+++ lib/pages/map_location.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatefulWidget {
-  const MapPage({Key? key}) : super(key: key);
-
-  @override
-  _MapPageState createState() => _MapPageState();
-}
-
-class _MapPageState extends State<MapPage> {
-  static const LatLng _pNorthEasternSeattle =
-      LatLng(47.62072920437455, -122.33745229057433);
+class MapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black, // Set background color to black
+        backgroundColor: Colors.white,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context); // Go back when button is pressed
+            Navigator.pop(context);
           },
         ),
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: _pNorthEasternSeattle,
-          zoom: 13,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/pages/map.jpeg'), // Ensure the path is correct
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Image.asset(
+            'lib/pages/image.png',
+            errorBuilder: (context, error, stackTrace) {
+              return Text('Error loading image: $error');
+            },
+          ),
         ),
       ),
     );
